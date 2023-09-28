db87b4155f70637bf746e8025b8ac190f7c77d0f
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2023-09-28 14:59:46 +0900

    feat: config server

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,14 +1,23 @@
 package com.example.orderservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
 
+    @Value("${test.data}")
+    private String data;
+
     @GetMapping("/test")
     public String test() {
         return "테스트 성공";
+    }
+
+    @GetMapping("/config-test")
+    public String testConfigClient() {
+        return "order config 테스트 성공 : " + data;
     }
 
 }
--- stock-service/src/main/java/com/example/stockservice/controller/StockController.java
+++ stock-service/src/main/java/com/example/stockservice/controller/StockController.java
@@ -1,14 +1,24 @@
 package com.example.stockservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StockController {
 
+    @Value("${test.data}")
+    private String data;
+
     @GetMapping("/test")
     public String test() {
         return "테스트 성공";
     }
+
+    @GetMapping("/config-test")
+    public String testConfigClient() {
+        return "stock config 테스트 성공 : " + data;
+    }
+
 
 }
